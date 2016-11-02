4c9d9e565ff0b7e7aa698a7c56e4fcae64f3bd77
fcostaa <felipe.h.j.costa@gmail.com>
2016-11-02 20:23:21 -0200

    Close the resource stream in DefaultRender after read it

--- src/main/kotlin/com/felipecosta/microservice/server/renderer/impl/DefaultRenderer.kt
+++ src/main/kotlin/com/felipecosta/microservice/server/renderer/impl/DefaultRenderer.kt
@@ -1,10 +1,8 @@
 package com.felipecosta.microservice.server.renderer.impl
 
 import com.felipecosta.microservice.server.renderer.Renderer
-import java.io.File
 
 class DefaultRenderer : Renderer {
-    override fun render(outputObject: Any, path: String): String {
-        return javaClass.classLoader.getResourceAsStream(path).reader().readText()
-    }
+    override fun render(outputObject: Any, path: String): String = javaClass.
+            classLoader.getResourceAsStream(path).reader().use { it.readText() }
 }
