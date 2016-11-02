86f7c5455caa2eea25d0f0656f8680d03a22739b
fcostaa <felipe.h.j.costa@gmail.com>
2016-11-02 17:50:06 -0200

    Map the movies API into Server

--- src/main/kotlin/com/felipecosta/microservice/Bootstrap.kt
+++ src/main/kotlin/com/felipecosta/microservice/Bootstrap.kt
@@ -1,5 +1,8 @@
 package com.felipecosta.microservice
 
+import com.felipecosta.microservice.app.core.di.DaggerApplicationComponent
+import com.felipecosta.microservice.app.movies.di.DaggerMoviesComponent
+import com.felipecosta.microservice.app.movies.frontcontroller.MoviesFrontCommand
 import com.felipecosta.microservice.server.*
 import com.felipecosta.microservice.server.frontcontroller.impl.HelloSparkFrontCommand
 import com.felipecosta.microservice.server.frontcontroller.impl.JsonSparkFrontCommand
@@ -10,9 +13,19 @@ import com.mitchellbosecke.pebble.PebbleEngine
 fun main(args: Array<String>) {
     val pebbleEngine = PebbleEngine.Builder().build()
 
+    val applicationComponent = DaggerApplicationComponent.builder().build()
+
     server {
         +(map get "/" to ::HelloSparkFrontCommand)
         +(map get "/json" to ::JsonSparkFrontCommand)
         +(map get "/notes" to ::NotesFrontCommand with PebbleRenderer(pebbleEngine))
+        +(map get "/api/movies" to {
+            val moviesFrontCommand = MoviesFrontCommand()
+            val moviesComponent = DaggerMoviesComponent.builder().
+                    applicationComponent(applicationComponent).
+                    build()
+            moviesComponent.inject(moviesFrontCommand)
+            moviesFrontCommand
+        })
     }
 }
