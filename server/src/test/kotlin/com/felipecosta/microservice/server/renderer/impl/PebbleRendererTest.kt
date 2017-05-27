51157c50c7c140fa94f390e020b9ef7617b4c0e3
fcostaa <felipe.h.j.costa@gmail.com>
2017-05-27 19:55:52 -0300

    Replace org.mockito.Matchers by the ones from mockito_kotlin

--- server/src/test/kotlin/com/felipecosta/microservice/server/renderer/impl/PebbleRendererTest.kt
+++ server/src/test/kotlin/com/felipecosta/microservice/server/renderer/impl/PebbleRendererTest.kt
@@ -2,9 +2,9 @@ package com.felipecosta.microservice.server.renderer.impl
 
 import com.mitchellbosecke.pebble.PebbleEngine
 import com.mitchellbosecke.pebble.template.PebbleTemplate
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.mock
-import org.mockito.Matchers.any
-import org.mockito.Matchers.eq
 import java.io.StringWriter
 
 class PebbleRendererTest {
@@ -25,7 +25,7 @@ class PebbleRendererTest {
         val outputObject = OutputObject("title", "content")
 
         com.nhaarman.mockito_kotlin.whenever(pebbleEngine.getTemplate(eq("index.html"))).thenReturn(pebbleTemplate)
-        com.nhaarman.mockito_kotlin.whenever(pebbleTemplate.evaluate(any(StringWriter::class.java), eq(mapOf("outputobject" to outputObject)))).then {
+        com.nhaarman.mockito_kotlin.whenever(pebbleTemplate.evaluate(any(), eq(mapOf("outputobject" to outputObject)))).then {
             val stringWriter = it.arguments[0] as StringWriter
             stringWriter.append("awesome text")
         }
@@ -40,7 +40,7 @@ class PebbleRendererTest {
         val outputMap = mapOf("title" to "My First Website", "content" to "My Interesting Content")
 
         com.nhaarman.mockito_kotlin.whenever(pebbleEngine.getTemplate(eq("index.html"))).thenReturn(pebbleTemplate)
-        com.nhaarman.mockito_kotlin.whenever(pebbleTemplate.evaluate(any(StringWriter::class.java), eq(outputMap))).then {
+        com.nhaarman.mockito_kotlin.whenever(pebbleTemplate.evaluate(any(), eq(outputMap))).then {
             val stringWriter = it.arguments[0] as StringWriter
             stringWriter.append("awesome text")
         }
@@ -55,7 +55,7 @@ class PebbleRendererTest {
         val outputObject = emptyMap<String, Any>()
 
         com.nhaarman.mockito_kotlin.whenever(pebbleEngine.getTemplate(eq("index.html"))).thenReturn(pebbleTemplate)
-        com.nhaarman.mockito_kotlin.whenever(pebbleTemplate.evaluate(any(StringWriter::class.java))).then {
+        com.nhaarman.mockito_kotlin.whenever(pebbleTemplate.evaluate(any())).then {
             val stringWriter = it.arguments[0] as StringWriter
             stringWriter.append("awesome text")
         }
