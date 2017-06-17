6d4b61e32102aba6137cbcfd2204a3873cdea1d9
fcostaa <felipe.h.j.costa@gmail.com>
2017-06-16 23:10:42 -0300

    Fix test name

--- app/src/test/kotlin/com/felipecosta/microservice/app/movies/frontcontroller/CreateMovieFrontCommandTest.kt
+++ app/src/test/kotlin/com/felipecosta/microservice/app/movies/frontcontroller/CreateMovieFrontCommandTest.kt
@@ -19,7 +19,7 @@ class CreateMovieFrontCommandTest {
     }
 
     @Test
-    fun givenJsonBody() {
+    fun givenJsonBodyThenReturnCreatedMovie() {
         frontCommand.init(object : Request by NotImplementedRequest() {
             override val body: String = """{"name":"New movie"}"""
         })
