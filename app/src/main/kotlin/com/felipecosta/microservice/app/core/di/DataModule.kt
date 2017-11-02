6b4e9e5f6f537c84a53847e0cdf07edefafb9abe
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-11-01 23:52:19 -0200

    Replace stub repository by redis implementation

--- app/src/main/kotlin/com/felipecosta/microservice/app/core/di/DataModule.kt
+++ app/src/main/kotlin/com/felipecosta/microservice/app/core/di/DataModule.kt
@@ -1,6 +1,6 @@
 package com.felipecosta.microservice.app.core.di
 
-import com.felipecosta.microservice.app.core.data.StubMovieRepository
+import com.felipecosta.microservice.app.core.data.RedisMoviesRepository
 import com.felipecosta.microservice.app.core.domain.MoviesRepository
 import dagger.Module
 import dagger.Provides
@@ -9,7 +9,11 @@ import javax.inject.Singleton
 @Module
 class DataModule {
 
+    companion object {
+        const val redisUri = "h:p4d9bad74864deada66ebed2e832c6d6bf2de394afef54902351c836ae9850e0e@ec2-54-227-223-104.compute-1.amazonaws.com:60759"
+    }
+
     @Singleton
     @Provides
-    fun provideMoviesRepository(): MoviesRepository = StubMovieRepository()
+    fun provideMoviesRepository(): MoviesRepository = RedisMoviesRepository(redisUri)
 }
