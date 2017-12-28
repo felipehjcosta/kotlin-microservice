29c82ea3821b53af1cd31a18c62588f04b803614
Felipe Costa <felipe.h.j.costa@gmail.com>
2017-12-28 08:07:05 -0200

    Replace mockito_kotlin with mockk in HelloFrontCommandTest

--- app/src/test/kotlin/com/felipecosta/microservice/app/helloworld/frontcontroller/HelloFrontCommandTest.kt
+++ app/src/test/kotlin/com/felipecosta/microservice/app/helloworld/frontcontroller/HelloFrontCommandTest.kt
@@ -2,20 +2,15 @@ package com.felipecosta.microservice.app.helloworld.frontcontroller
 
 import com.felipecosta.microservice.server.Request
 import com.felipecosta.microservice.server.Response
-import com.nhaarman.mockito_kotlin.mock
-import org.junit.Test
-import kotlin.test.assertEquals
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class HelloFrontCommandTest {
 
-    val request: Request = mock()
+    private val request = mockk<Request>()
 
-    lateinit var helloSparkFrontCommand: HelloFrontCommand
-
-    @org.junit.Before
-    fun setUp() {
-        helloSparkFrontCommand = HelloFrontCommand().apply { init(request) }
-    }
+    private val helloSparkFrontCommand = HelloFrontCommand().apply { init(request) }
 
     @Test
     fun whenProcessThenAssertResponse() {
