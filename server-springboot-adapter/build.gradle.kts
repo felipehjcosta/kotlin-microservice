5ea78cde1e4ce7d7569d0af1c97bc4eea3bb623b
Felipe Costa <felipe.h.j.costa@gmail.com>
2018-11-28 23:51:22 -0200

    Bump SpringBoot dependencies

--- server-springboot-adapter/build.gradle.kts
+++ server-springboot-adapter/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     compile(project(":server"))
-    compile("org.springframework.boot:spring-boot-starter-web:1.5.3.RELEASE")
-    testCompile("org.springframework.boot:spring-boot-starter-test:1.5.3.RELEASE")
+    compile("org.springframework.boot:spring-boot-starter-web:2.0.4.RELEASE")
+    testCompile("org.springframework.boot:spring-boot-starter-test:2.0.4.RELEASE")
     testCompile("com.nhaarman:mockito-kotlin:1.3.0") {
         exclude(group = "org.jetbrains.kotlin")
     }
